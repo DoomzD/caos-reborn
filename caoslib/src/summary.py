400748a124c8e66559a168edba1adec270bf1301
DoomzD <doomzdayforwork@gmail.com>
2019-11-17 00:44:03 +0300

    Add summarized info to summary

--- caoslib/src/summary.py
+++ caoslib/src/summary.py
@@ -8,12 +8,17 @@ def summary(session, show_solved):
     problems = get_problems(session)
 
     problem_template = "{: <32}{}({})"
+    problems_solved = 0
+    total_score = 0
     for problem in problems:
         formatted_problem = problem_template.format(
             problem['name'] + ":", 
             problem['status'], 
             problem['score']
         )
+
+        total_score += problem['score']
+        problems_solved += problem['status'] in [OK, REVIEW] 
 
         if problem['status'] == OK and show_solved:
             with indent(4, quote='✔'):
@@ -24,3 +29,8 @@ def summary(session, show_solved):
         elif problem['status'] not in [OK, REVIEW]:
             with indent(4, quote='✖'):
                 puts(colored.red(formatted_problem))
+    
+    puts('{:_>50}'.format(''))
+    puts(colored.green(
+        f'Total tasks: {len(problems)}, Solved tasks: {problems_solved}, Total score: {total_score}'
+    ))
