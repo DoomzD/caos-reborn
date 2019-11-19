0b74172527454298732e902e4a529eea3923fc34
DoomzD <doomzdayforwork@gmail.com>
2019-11-19 15:26:09 +0300

    Add compiler string

--- caoslib/utils/constants.py
+++ caoslib/utils/constants.py
@@ -4,7 +4,7 @@ import os
 CONFIG_PATH = os.getcwd() + '/caoslib/files/config.ini'
 COOKIES_PATH = os.getcwd() + '/caoslib/files/cookies.owo'
 LINKS_PATH = os.getcwd() + '/caoslib/files/links.json'
-CAOS_DIR = os.getenv('HOME') + '/programming/caos'
+CAOS_DIR = os.getenv('HOME') + '/programming/hse/caos'
 
 # links
 SETTINGS = 'Settings'
@@ -18,3 +18,5 @@ CLARS = 'Clars'
 OK = 'OK'
 REVIEW = 'Pending review'
 NOT_SUBMITTED = 'Not submitted'
+
+COMPILATION_STRING = "gcc -O2 -Wall -Werror -Wno-unused-result -std=gnu11 -lm {}"
