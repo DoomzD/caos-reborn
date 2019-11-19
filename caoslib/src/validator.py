4d8b6c3d34d292b9d48f6123ed7308d7d57c21c6
DoomzD <doomzdayforwork@gmail.com>
2019-11-19 12:30:45 +0300

    Add cookies validation

--- caoslib/src/validator.py
+++ caoslib/src/validator.py
@@ -1,8 +1,16 @@
-from utils.constants import SUMMARY
+from utils.constants import COOKIES_PATH, SUMMARY
 from utils.utils import read_links
 
 from clint.textui import puts, colored
 import json
+
+
+def validate_cookies():
+    try:
+        open(COOKIES_PATH, 'r').close()
+    except:
+        puts(colored.red("No cookies found. Run './caos init-session'."))
+        exit(1)
 
 
 def validate_session(session):
