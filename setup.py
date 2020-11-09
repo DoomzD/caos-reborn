0e3733bb91b8eca56f0e15b37139ad9a629310a7
Melnikov Artem <melnikovam10@gmail.com>
2020-11-10 00:35:07 +0300

    i fucking commited temporary movement

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name = 'caos-reborn',
-    version = '1.5.3',
+    version = '1.6.1',
     url = 'https://github.com/DoomzD/caos-reborn',
-    author = 'Alex Koryakov & Artem Melnikov',
-    author_email = 'avkoryakov@gmail.com & melnikovam10@gmail.com',
+    author = 'Artem Melnikov & Alex Koryakov',
+    author_email = 'melnikovam10@gmail.com',
     description = 'Multifunctional tool for caos course',
     packages = find_packages(),
     scripts = ['caos'],
