3df7770c331b6420bb91210c2b6f50945719798b
Melnikov Artem <melnikovam10@gmail.com>
2020-11-17 14:14:38 +0300

    1.9 build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'caos-reborn',
-    version = '1.8.4',
+    version = '1.9.0',
     url = 'https://github.com/DoomzD/caos-reborn',
     author = 'Artem Melnikov & Alex Koryakov',
     author_email = 'melnikovam10@gmail.com',
